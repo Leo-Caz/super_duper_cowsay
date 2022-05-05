6e9230cefa3a1d9c8d97aab3dc555838f088cdeb
Léo Cazenave <leo@cazenave.cc>
2022-05-05 11:01:15 +0200

    Tamagochi-cow jouable mais un peu moche.

--- tamagochi_cow.c
+++ tamagochi_cow.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "generer_entier.c"
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define CLAMP(x, min, max) MAX(min, MIN(x, max))
+
+#define BYEBYELIFE 0
+#define LIFESUCKS  1
+#define LIFEROCKS  2
 
 
+// Demande à l'utilisateur la quantité de nourriture à donner à la vache
 int lunch_food(int stock) {
 	int reponse_valide = 0;
 	char reponse[100];
@@ -11,8 +19,7 @@ int lunch_food(int stock) {
 
 	while (reponse_valide == 0) {
 		// On récupère la réponse de l'utilisateur
-		printf("Vous voulez donner combien de bouffe à la poly-vache ?\n");
-		printf("(entre 0 exclu et %d inclus)\n -> ", stock);
+		printf("Vous voulez donner combien de bouffe à la poly-vache ?\n -> ");
 		fscanf(stdin, "%s", reponse);
 
 		// On récupère les nombres dans la chaîne de caractères
@@ -27,13 +34,76 @@ int lunch_food(int stock) {
 
 	return bouffe_demande;
 }
+
+
+void tmp_affiche_vache(int etat_vache) {
+	switch (etat_vache) {
+		case LIFEROCKS:
+			printf("Yooooooo, comment chuis grave bien làààààà.....\n");
+			break;
+
+		case LIFESUCKS:
+			printf("Chuis pas venu ici pour souffrir OKAAAY!!!\n");
+			break;
+
+		case BYEBYELIFE:
+			printf("Fuck le capitalisme\n");
+			break;
+	}
+}
+
+
+int stock_update(int stock, int nourriture_donne) {
+	// crop compris entre -3 et 3
+	int crop = (rand() % 6) - 3;
+	printf("crop: %d\n", crop);
+	int nouveau_stock = stock + crop - nourriture_donne;
+	return CLAMP(nouveau_stock, 0, 10);
+}
+
+
+int fitness_update(int fitness, int nourriture_donne) {
+	// digestion compris entre 0 et 3
+	int digestion = rand() % 3;
+	printf("digestion: %d\n", digestion);
+	int nouvelle_fitness = fitness + nourriture_donne - digestion;
+	return CLAMP(nouvelle_fitness, 0, 10);
+}
 		
 
-
-
 int main() {
-	int fitness = 5;
 	int stock = 5;
-	long digestion = -1 * generer_entier(4);
-	/* int bite = lunch_food(stock); */
+	int fitness = 5;
+	int etat_vache = LIFEROCKS;
+	int nourriture_donnee;
+	unsigned int duree_de_vie = 0;
+
+	tmp_affiche_vache(etat_vache);
+
+	while (etat_vache != BYEBYELIFE) {
+		if (stock == 0) {
+			printf("Vous n'avez plus de nourriture dans le stock, le tour est passé\n");
+			nourriture_donnee = 0;
+		} else {
+			printf("Il vous reste %d nourritures dans le stock\n", stock);
+			nourriture_donnee = lunch_food(stock);
+		}
+		stock = stock_update(stock, nourriture_donnee);
+		fitness = fitness_update(fitness, nourriture_donnee);
+		duree_de_vie++;
+
+		if (fitness >= 4 && fitness <= 6) {
+			etat_vache = LIFEROCKS;
+		} else if (fitness >= 1 && fitness <= 9) {
+			etat_vache = LIFESUCKS;
+		} else {
+			etat_vache = BYEBYELIFE;
+		}
+
+		// Affichage de l'état de la vache
+		printf("______________________\n");
+		tmp_affiche_vache(etat_vache);
+	}
+
+	printf("Votre vache à vécu pendant %d tours\n", duree_de_vie);
 }
